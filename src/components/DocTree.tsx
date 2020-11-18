219d8904579095b762a20cf8658c14e763a1f9c6
Mathis Bullinger <mathis@bullinger.dev>
2020-11-18 01:06:15 +0100

    feat: display nodes & hover

--- src/components/DocTree.tsx
+++ src/components/DocTree.tsx
@@ -7,18 +7,42 @@ type Props = {
 }
 
 function parse(node: Node, i?: number) {
-  const children = Array.from(node.childNodes).map(parse)
+  const children = Array.from(node.childNodes)
+  let childContent = ''
+  if (children.length === 1 && children[0].nodeName === '#text')
+    childContent = children[0].textContent ?? ''
+  const tag = (node as Element).tagName
+
+  const head =
+    node.nodeName === '#text'
+      ? node.textContent
+      : tag
+      ? `<${tag}>`
+      : node.nodeName
+
+  const close = tag && <CloseTag>&lt;{tag}&gt;</CloseTag>
+
+  if (childContent)
+    return (
+      <Node key={i}>
+        {head}
+        {childContent}
+        {close}
+      </Node>
+    )
   return (
     <Node key={i}>
-      {node.nodeName}
-      {children.length > 0 && <ol>{children}</ol>}
+      {head}
+      {children.length > 0 && <ol>{children.map(parse)}</ol>}
+      {close}
     </Node>
   )
 }
 
 const Node: React.FC = ({ children }) => {
-  const hasChildren =
-    (React.Children.toArray(children).slice(-1)[0] as any)?.type === 'ol'
+  const hasChildren = React.Children.toArray(children).some(
+    v => (v as any).type === 'ol'
+  )
   return (
     <S.Node
       role="treeitem"
@@ -46,8 +70,23 @@ export default function DocTree({ document }: Props) {
 
   const pretty = useComputed(document, parse)
 
-  return <S.Document>{pretty}</S.Document>
+  return (
+    <S.Document>
+      <S.Root
+        onMouseOver={({ target }) =>
+          (target as HTMLElement).toggleAttribute('data-hover')
+        }
+        onMouseOut={({ target }) =>
+          (target as HTMLElement).toggleAttribute('data-hover')
+        }
+      >
+        {pretty}
+      </S.Root>
+    </S.Document>
+  )
 }
+
+const CloseTag = styled.span``
 
 const S = {
   Document: styled.div`
@@ -56,23 +95,28 @@ const S = {
     }
   `,
 
+  Root: styled.div`
+    font-family: 'Menlo', 'Source Code Pro', monospace;
+    font-size: 14px;
+    line-height: 1.4em;
+  `,
+
   Node: styled.li`
     &[data-hover] {
       background-color: #fff1;
     }
 
-    &[aria-expanded='false'] {
-      &::before {
-        content: '> ';
-      }
-
-      & > ol {
-        display: none;
-      }
+    &[aria-expanded='false']::marker {
+      content: '▸ ';
     }
 
-    &[aria-expanded='true']::before {
-      content: 'x ';
+    &[aria-expanded='true']::marker {
+      content: '▾ ';
+    }
+
+    &[aria-expanded='false'] > ol,
+    &[aria-expanded='false'] > ${CloseTag} {
+      display: none;
     }
   `,
 }
