19de969d23b04496937333a6a373bfe788799d27
Mathis Bullinger <mathis@bullinger.dev>
2020-11-20 09:55:42 +0100

    feat: hsla parsing

--- src/utils/css/color/channels.ts
+++ src/utils/css/color/channels.ts
@@ -1,5 +1,7 @@
-export const compose = (r: number, g: number, b: number, a: number): RGBA =>
-  (r << 24) + (g << 16) + (b << 8) + a
+export const compose = (...ch: RGBA_): RGBA => {
+  const [r, g, b, a] = ch.map(v => Math.max(Math.min(Math.round(v), 0xff), 0))
+  return (r << 24) + (g << 16) + (b << 8) + a
+}
 
 export const split = (
   rgba: RGBA
@@ -9,4 +11,25 @@ export const split = (
   const b = (rgba >> 8) & 0xff
   const a = rgba & 0xff
   return [r, g, b, a]
+}
+
+export const hsla = (h: number, s: number, l: number, a: number): RGBA => {
+  let r, g, b
+  if (s === 0) r = g = b = l
+  else {
+    const hue2rgb = (p: number, q: number, t: number) => {
+      if (t < 0) t += 1
+      if (t > 1) t -= 1
+      if (t < 1 / 6) return p + (q - p) * 6 * t
+      if (t < 1 / 2) return q
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6
+      return p
+    }
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s
+    const p = 2 * l - q
+    r = hue2rgb(p, q, h + 1 / 3)
+    g = hue2rgb(p, q, h)
+    b = hue2rgb(p, q, h - 1 / 3)
+  }
+  return compose(...([r, g, b, a].map(v => v * 0xff) as RGBA_))
 }
--- src/utils/css/color/parse.ts
+++ src/utils/css/color/parse.ts
@@ -1,12 +1,23 @@
 import { expandHex } from './format'
 import named, { ColorName } from './named'
-import { compose } from './channels'
+import { compose, hsla } from './channels'
 
 export const parseHex = (v: string): RGBA => parseInt(v.slice(1), 16)
 
+export const parseAngle = (v: string): number => {
+  v = v.toLocaleLowerCase()
+  const unit = v.match(/[a-z]+(?=$)/)?.[0]
+  if (!unit) return parseFloat(v)
+  const n = parseFloat(v.slice(0, unit.length))
+  if (unit === 'deg') return n
+  if (unit === 'turn') return n * 360
+  if (unit === 'rad') return n * (180 / Math.PI)
+  throw Error(`unknown angular unit "${v}"`)
+}
+
 export const parse = (v: string): RGBA => {
   v = v.trim()
-  if (v.startsWith(v)) return parseHex(expandHex(v))
+  if (v.startsWith('#')) return parseHex(expandHex(v))
   if (v in named) return named[v as ColorName]
   const [func, rest] = v.split('(')
   const parts = rest.split(/[\s,/]+/g)
@@ -21,6 +32,15 @@ export const parse = (v: string): RGBA => {
         ),
         a.endsWith('%') ? parseFloat(v.slice(0, -1)) / 100 : parseFloat(a),
       ].map(v => Math.min(Math.max(v * 255, 0), 255)) as RGBA_)
+    )
+  }
+  if (/hsla?/.test(func)) {
+    const [h, s, l, a = '1'] = parts
+    return hsla(
+      parseAngle(h) / 360,
+      parseFloat(s) / 100,
+      parseFloat(l) / 100,
+      parseFloat(a)
     )
   }
   throw Error(`failed to parse "${v}" as color`)
