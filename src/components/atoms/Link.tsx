534992300ce7d4c7049cdb022b3fbb2295f3a42d
Mathis Bullinger <mathis@bullinger.dev>
2021-04-14 14:34:19 +0200

    feat: spa link independent of react router

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -1,20 +1,32 @@
 import React from 'react'
-import styled from 'styled-components'
 import { Link as RouterLink } from 'react-router-dom'
+import { setUrl } from 'utils/url'
+import { useComputed } from 'utils/hooks'
 
 type Props = {
   append?: boolean
   to: string
   wrap?: boolean
   nav?: boolean | string
+  independent?: boolean
 } & ReactProps<typeof RouterLink>
 
-export const Link = ({ children, to, append, wrap, nav, ...props }: Props) => {
+export const Link = ({
+  children,
+  to,
+  append,
+  wrap,
+  nav,
+  independent,
+  ...props
+}: Props) => {
   const isExternal = /^https?:\/\//.test(to)
   if (/^[?#]/.test(to)) to = location.pathname + to
 
+  const Comp: any = isExternal ? 'a' : independent ? Independent : RouterLink
+
   const link = (
-    <SLink
+    <Comp
       {...(isExternal
         ? {
             href: to,
@@ -23,25 +35,46 @@ export const Link = ({ children, to, append, wrap, nav, ...props }: Props) => {
             rel: 'noopener noreferrer',
           }
         : {
-            to: {
-              pathname: append
-                ? `${location.pathname}/${to}`.replace(/\/+/g, '/')
-                : to,
-              state: { previous: location.pathname + location.search },
-            },
+            to: independent
+              ? to
+              : {
+                  pathname: append
+                    ? `${location.pathname}/${to}`.replace(/\/+/g, '/')
+                    : to,
+                  state: { previous: location.pathname + location.search },
+                },
           })}
       {...(wrap && { style: { display: 'contents', textDecoration: 'none' } })}
       {...(nav && location.pathname === to && { 'aria-current': 'page' })}
       {...props}
     >
       {children}
-    </SLink>
+    </Comp>
   )
   if (typeof nav !== 'string') return link
   const Wrap: 'h1' = nav as any
   return <Wrap>{link}</Wrap>
 }
 
-const SLink = styled(RouterLink)`
-  color: var(--cl-text);
-`
+function Independent({ to, children }: Props) {
+  const url = useComputed(to, to => {
+    const [base, rest] = to.split(/[?#]/)
+    const path = base.startsWith('/')
+      ? base
+      : `${location.pathname.replace(/^\/$/, '')}/${base}`
+    const [qs, hash] = rest?.split('#')
+    return { path, query: qs?.replace(/^\?/, ''), hash }
+  })
+
+  return (
+    <a
+      href={to}
+      onClick={e => {
+        e.preventDefault()
+        setUrl(url)
+      }}
+    >
+      {children}
+    </a>
+  )
+}
