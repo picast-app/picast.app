50832eacdd79f107e1c644e379806332c6884fce
Mathis Bullinger <mathis@bullinger.dev>
2021-05-14 16:55:25 +0200

    feat: basic shownote styling

--- src/components/composite/Shownotes.tsx
+++ src/components/composite/Shownotes.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import styled from 'styled-components'
 
 type Props = React.HTMLProps<HTMLDivElement> & {
   children?: string
 }
 
-export function Shownotes({ children: __html = '', ...props }: Props) {
-  return <div dangerouslySetInnerHTML={{ __html }} {...props} />
-}
+export const Shownotes: React.FC<Props> = ({
+  children: __html = '',
+  ...props
+}) => (
+  // @ts-ignore
+  <Notes
+    dangerouslySetInnerHTML={{ __html }}
+    {...props}
+    ref={el =>
+      el?.querySelectorAll<HTMLAnchorElement>('a').forEach(a => {
+        a.setAttribute('target', '_blank')
+        a.setAttribute('rel', 'noopener noreferrer')
+      })
+    }
+  />
+)
+
+const Notes = styled.div`
+  line-height: 1.2;
+
+  a {
+    color: var(--cl-primary);
+  }
+
+  * ~ p {
+    margin-top: 0.7em;
+  }
+`
