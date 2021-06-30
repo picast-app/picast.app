d8cd7ecda672a8564338287334e0ab9a8cdbc6e3
Mathis Bullinger <mathis@bullinger.dev>
2021-06-30 17:13:35 +0200

    ref: sandbox routing

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -10,9 +10,6 @@ const Profile = React.lazy(() => import('screens/Profile'))
 const Search = React.lazy(() => import('screens/Search'))
 const Podcast = React.lazy(() => import('screens/Podcast'))
 const Sandbox = React.lazy(() => import('screens/Sandbox'))
-const Elevation = React.lazy(() => import('screens/sandbox/Elevation'))
-const Ease = React.lazy(() => import('screens/sandbox/Ease'))
-const Components = React.lazy(() => import('screens/sandbox/Components'))
 const FeedView = React.lazy(() => import('screens/FeedView'))
 const SignIn = React.lazy(() => import('screens/SignIn'))
 const Settings = React.lazy(() => import('screens/Settings'))
@@ -30,10 +27,7 @@ export default () => (
         <Route path="/show/:id">{Podcast}</Route>
         <Route path="/sign(in|up)">{SignIn}</Route>
         <Route path="/settings/:page?">{Settings}</Route>
-        <Route path="/sandbox">{Sandbox}</Route>
-        <Route path="/sandbox/elevation">{Elevation}</Route>
-        <Route path="/sandbox/ease">{Ease}</Route>
-        <Route path="/sandbox/components">{Components}</Route>
+        <Route path="/sandbox/:sub?">{Sandbox}</Route>
         <Route path="/feedview/.*">{FeedView}</Route>
         <Route path="/404">{S404}</Route>
         <Redirect to="/" />
--- src/screens/Sandbox.tsx
+++ src/screens/Sandbox.tsx
@@ -1,8 +1,23 @@
 import React, { useState } from 'react'
+import { Switch, Route, Redirect, Link } from '@picast-app/router'
 import { Screen } from 'components/structure'
-import { Link } from '@picast-app/router'
+import Elevation from './sandbox/Elevation'
+import Ease from './sandbox/Ease'
+import Components from './sandbox/Components'
 
-export default function Sandbox() {
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/sandbox">{Sandbox}</Route>
+      <Route path="/sandbox/elevation">{Elevation}</Route>
+      <Route path="/sandbox/ease">{Ease}</Route>
+      <Route path="/sandbox/components">{Components}</Route>
+      <Redirect to="/sandbox" />
+    </Switch>
+  )
+}
+
+function Sandbox() {
   const [loading, setLoading] = useState(false)
 
   return (
