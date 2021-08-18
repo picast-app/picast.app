b78fdbba86604412bec7486a1feb8bd11aa7be4c
Mathis Bullinger <mathis@bullinger.dev>
2021-08-18 18:08:40 +0200

    feat: shownotes

--- src/store/handlers/episodePodMap.ts
+++ src/store/handlers/episodePodMap.ts
@@ -1,12 +1,23 @@
+import dbProm from 'main/idb/idb'
 import type { Store } from 'store'
 
 export default (store: Store) => {
   const map = new Map<string, string>()
-  store.handler('ep2Pod.*').get((_, id) => map.get(id))
+  store
+    .handler('ep2Pod.*')
+    .get(async (_, id) => map.get(id) ?? (await searchId(id)))
   store.handler('ep2Pod.*').set((pod, p, m, ep) => {
     if (pod) map.set(ep, pod)
   })
   store.listen('player.current', id => {
     if (id) store.set('ep2Pod.*', id[0], {}, id[1])
   })
+
+  async function searchId(id: string) {
+    const idb = await dbProm
+    const res = await idb.get('episodes', id)
+    if (!res?.podcast) return
+    store.set('ep2Pod.*', res.podcast, {}, id)
+    return res.podcast
+  }
 }
--- src/store/handlers/shownotes.ts
+++ src/store/handlers/shownotes.ts
@@ -1,16 +1,16 @@
 import type { Store } from 'store'
 import * as api from 'api/calls'
 import idb from 'main/idb/idb'
+import { asyncNullishChain } from 'snatchblock/nullishChain'
 
 export default (store: Store) => {
-  store.handler('episodes.*.shownotes').get(
-    async (_, e) => {
-      return ''
-    }
-    // asyncNullChain(readShowNotes, queryShownotes)([p, e])
-  )
+  store.handler('episodes.*.shownotes').get(async (_, epId) => {
+    const podId = await store.get('ep2Pod.*', epId)
+    if (!podId) return "couldn't fetch show notes"
+    return await asyncNullishChain(readShowNotes, queryShownotes)([podId, epId])
+  })
 
-  store.handler('episodes.*.shownotes').set(async (shownotes, p, m, _, id) => {
+  store.handler('episodes.*.shownotes').set(async (shownotes, p, m, id) => {
     const db = await idb
     shownotes ??= (await db.get('episodeInfo', id))?.shownotes
     await db.put('episodeInfo', { id, shownotes, fetched: Date.now() })
